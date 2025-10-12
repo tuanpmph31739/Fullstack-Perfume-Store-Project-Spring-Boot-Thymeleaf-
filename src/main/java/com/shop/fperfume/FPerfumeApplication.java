58a7253a439801ed3179d5326fe0e1855ac966d1
tuanpmph41739 <tuanpmph31739@fpt.edu.vn>
2025-10-12 20:42:51 +0700

    First commit

---
+++ src/main/java/com/shop/fperfume/FPerfumeApplication.java
@@ -0,0 +1,13 @@
+package com.shop.fperfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FPerfumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FPerfumeApplication.class, args);
+    }
+
+}
