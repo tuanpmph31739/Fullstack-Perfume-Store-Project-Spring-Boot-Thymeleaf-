f7128234e5bedbd303fe5c8c6e80c7483453223a
manh <manhech1608@gmail.com>
2025-10-22 22:53:05 +0700

    abc

--- src/main/java/com/shop/fperfume/service/SanPhamService.java
+++ src/main/java/com/shop/fperfume/service/SanPhamService.java
@@ -1,10 +1,13 @@
 package com.shop.fperfume.service;
 
+import com.shop.fperfume.entity.DungTich;
 import com.shop.fperfume.entity.LoaiNuocHoa;
 import com.shop.fperfume.entity.SanPham;
+import com.shop.fperfume.model.request.SanPhamRequest;
 import com.shop.fperfume.model.response.PageableObject;
 import com.shop.fperfume.model.response.SanPhamResponse;
 import com.shop.fperfume.repository.*;
+import com.shop.fperfume.util.MapperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -44,4 +47,12 @@ public class SanPhamService {
         return new PageableObject<>(responses);
     }
 
+    public void addSanPham(SanPhamRequest sanPhamRequest){
+        SanPham sanPham = MapperUtils.map(sanPhamRequest, SanPham.class);
+
+        DungTich dungTich = dungTichRepository.findById(sanPhamRequest.getIdDungTich()).orElseThrow(() -> new RuntimeException("Không tìm thấy dung tích với ID: " + sanPhamRequest.getIdDungTich()));
+        sanPham.setDungTich(dungTich);
+
+        sanPhamRepository.save(sanPham);
+    }
 }
